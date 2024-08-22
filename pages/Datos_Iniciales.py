2d853c9e5cb8f9822f2c07d334790c0f5493166c
Arquero <PC@arch.linux>
2024-08-21 22:04:50 -0400

    deleted scipy

--- pages/Datos_Iniciales.py
+++ pages/Datos_Iniciales.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import scipy
 import pandas as pd
 import math as math
 import plotly.express as px
--- pages/Datos_Limpios.py
+++ pages/Datos_Limpios.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import scipy
 import seaborn as sns
 from matplotlib import pyplot
 import pandas as pd
